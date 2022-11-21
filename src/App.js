7580b63de55d944357209263461dbb6a9f232c69
Simeon Rangelov <67736865+Mr-Sims@users.noreply.github.com>
2022-11-21 20:21:05 +0200

    Added main components
    -Router set up
    -Home view set up
    -Header and navigation set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Catalogue from './components/Catalogue/Catalogue';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Create from './components/Create/Create';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div id="box">
+
+			<Header />
+
+			{/* Main Content */}
+			<main id="main-content">
+
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/login' element={<Login />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/catalogue' element={<Catalogue/>} />
+					<Route path='/create' element={<Create />} />
+				</Routes>
+
+			</main>
+
+			{/* Login Page ( Only for Guest users ) */}
+			{/* Register Page ( Only for Guest users ) */}
+			{/* Create Page ( Only for logged-in users ) */}
+			{/* Edit Page ( Only for the creator )*/}
+			{/*Details Page*/}
+			{/* Catalogue */}
+
+		</div>
+	);
 }
 
 export default App;
